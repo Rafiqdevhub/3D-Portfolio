134f971f35422af71e7c1b7016aad9d095435c94
MUhammad Rafiq <rafiq85825@gmail.com>
2025-04-12 22:26:46 +0500

    Add Footer component to enhance page structure and include social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { NavBar } from "./components/NavBar";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <NavBar />
       <Hero />
+      <Footer />
     </>
   );
 };
