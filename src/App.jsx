9733a44e2accdb49a52092c983686047da2edf7f
MUhammad Rafiq <rafiq85825@gmail.com>
2025-04-13 16:33:12 +0500

    finished the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { NavBar } from "./components/NavBar";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
+import Showcase from "./sections/ShowcaseSection";
+import NavBar from "./components/NavBar";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCard";
+import Experience from "./sections/Experiences";
+import TechStack from "./sections/TechStack";
+import Contact from "./sections/Contact";
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <Showcase />
+      <LogoShowcase />
+      <FeatureCards />
+      <Experience />
+      <TechStack />
+      <Contact />
       <Footer />
     </>
   );
